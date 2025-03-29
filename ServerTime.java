cf3b3782f3df35f0c0703afdd51c21bd26d64cc6
Tewodros <thedronberhanu05@gmail.com>
2025-03-29 11:49:23 +0300

    git commit -m srt

--- ServerTime.java
+++ ServerTime.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.devtools.DevTools;
 // import org.openqa.selenium.devtools.v134.network.Network;
 // import org.openqa.selenium.devtools.v134.network.model.Response;
-
-
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
